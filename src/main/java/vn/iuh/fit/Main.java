2bb3635c93049331368bebcaf7f33b90d1cc1043
TriNguyen <ntti2k2@gmail.com>
2024-03-04 19:40:43 +0700

    Code oke

--- src/main/java/vn/iuh/fit/Main.java
+++ src/main/java/vn/iuh/fit/Main.java
@@ -1,7 +1,16 @@
 package vn.iuh.fit;
 
+import jdepend.xmlui.JDepend;
+
+import java.io.PrintWriter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello");
+
+        JDepend jDepend = new JDepend(new PrintWriter("reports/report.xml"));
+        jDepend.addDirectory("T:\\Library-Assistant-master");
+        jDepend.analyze();
+        System.out.println("DONE");
     }
 }
